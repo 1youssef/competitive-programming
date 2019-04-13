a34926c806f15ee33c896d324b8bc99d3b5f0c12
Yousef Murad <yosef.murad@gmail.com>
2019-04-13 13:29:34 +0800

    Create M Amine The Farmer.cpp

---
+++ IIUM - ICPC 2019 Selection Contest/Day 1/M Amine The Farmer.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define sz(x) (int)x.size()
+#define sf(n) scanf(" %d",&n)
+#define pf(n)  printf("%d", n)
+#define all(v) ((v).begin(), (v).end())
+#define pb(n)  push_back(n)
+
+const double eps = 1e-6;
+
+ll pw(ll b, ll p){ if (!p) return 1; ll sq = pw(b, p / 2); sq *= sq; if (p % 2) sq *= b; return sq; }
+
+int main()
+{
+   int n, m, p;
+    cin >> n >> m >> p;
+    cout << ((2*n + 2*m) * p)<< " RM\n";
+    return 0;
+}
